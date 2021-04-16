bfefa73ed4f939811d3075303d118e579a05734e
advaitha jayakumar <advaithajayakumar@gmail.com>
2021-04-16 21:15:06 +0530

    second matrix defined

--- matrix multiplication.py
+++ matrix multiplication.py
@@ -5,4 +5,10 @@ for i in range(row1):
     for j in range(col1):
         max1[i][j]=int(input("enter the element: "))
 print(max1)
-
+row2=int(input("enter the number of rows in 2nd matrix"))
+col2=int(input("enter the number of columns of 2nd matrix"))
+max2=[[[]for j in range(col2)]for i in range(row2)]
+for i in range(row2):
+    for j in range(col2):
+        max2=int(input("enter the element"))
+print(max2)
